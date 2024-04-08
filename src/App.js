4a940e84afea7c6aeed9d5fb9bf8cea09b1b5e31
Raheman Ali <49222155+Raheman-08@users.noreply.github.com>
2024-04-08 14:22:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Category from './screens/Category';
+
+export default function App() {
+  return(
+    <Category />
+  );
+}
---
+++ src/screens/Category.js
@@ -0,0 +1,19 @@
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Category = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>category</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+    }
+})
+
+export default Category
